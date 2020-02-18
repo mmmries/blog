9c32604df805d235b26962234ec43560d01258f5
mmmries <riesmmm@gmail.com>
2020-02-18 14:27:45 -0600

    use a GenServer to hold the text index

--- lib/blog/application.ex
+++ lib/blog/application.ex
@@ -9,9 +9,9 @@ defmodule Blog.Application do
     # List all child processes to be supervised
     children = [
       # Start the endpoint when the application starts
-      BlogWeb.Endpoint
+      BlogWeb.Endpoint,
       # Starts a worker by calling: Blog.Worker.start_link(arg)
-      # {Blog.Worker, arg},
+      {Blog.SearchServer, nil}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/blog/search_server.ex
@@ -0,0 +1,32 @@
+defmodule Blog.SearchServer do
+  use GenServer
+  require Logger
+
+  def query(string) do
+    GenServer.call(__MODULE__, {:query, string})
+  end
+
+  def start_link(nil) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(nil) do
+    {:ok, nil, {:continue, :build_index}}
+  end
+
+  def handle_continue(:build_index, _state) do
+    {micros, index} = :timer.tc(fn ->
+      Blog.Search.build_index(Blog.list_posts())
+    end)
+    Logger.info("it took #{micros}µs to build the text index")
+    {:noreply, index}
+  end
+
+  def handle_call({:query, query}, _from, index) do
+    {micros, response} = :timer.tc(fn ->
+      Blog.Search.search(index, query)
+    end)
+    Logger.info("it took #{micros}µs to query")
+    {:reply, response, index}
+  end
+end
