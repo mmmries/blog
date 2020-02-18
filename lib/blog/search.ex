7109db697d8cd2491b2c8b9b1929bc2156ad49e1
mmmries <riesmmm@gmail.com>
2020-02-18 14:27:25 -0600

    extra the logic for text searching to its own module

---
+++ lib/blog/search.ex
@@ -0,0 +1,40 @@
+defmodule Blog.Search do
+  def build_index(posts) do
+    posts
+    |> Enum.reduce(Bayesic.Trainer.new(), fn(post, trainer) ->
+      tokens = post.body |> Floki.parse_fragment!() |> Floki.text() |> tokenize()
+      Bayesic.train(trainer, tokens, post)
+    end)
+    |> Bayesic.finalize(pruning_threshold: 0.5)
+  end
+
+  @min_probability 0.2
+  def search(index, query, max_matches \\ 5) do
+    Bayesic.classify(index, tokenize(query))
+    |> Enum.filter(fn({_post, probability})->
+      probability > @min_probability
+    end)
+    |> Enum.sort_by(fn({_post, probability}) ->
+      probability * -1
+    end)
+    |> Enum.take(max_matches)
+    |> Enum.map(fn({post, _probability}) -> post end)
+  end
+
+  def tokenize(str) do
+    Regex.scan(~r{\b\w+\b}, str)
+    |> Enum.reduce(MapSet.new(), fn(matches, set) ->
+      word = hd(matches)
+      set = word |> Stemmer.stem() |> trigrams() |> Enum.into(set)
+      set = word |> trigrams() |> Enum.into(set)
+      set
+    end)
+  end
+
+  def trigrams(word) do
+    word
+    |> String.graphemes()
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.join/1)
+  end
+end
