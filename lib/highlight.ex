2431e9f67603e8c3fe0096733a028375788d5d59
Michael Ries <riesmmm@gmail.com>
2021-11-23 21:10:46 +0000

    simplify Highlight module

--- lib/highlight.ex
+++ lib/highlight.ex
@@ -21,46 +21,4 @@ defmodule Highlight do
     #{:ok, highlighted} = code(filename, code |> unescape_html() |> IO.iodata_to_binary())
     ~s(<pre class="language-#{lang}"><code class="language-#{lang}">#{code}</code></pre>)
    end
-
-   defp sample_filename("ruby"), do: "example.rb"
-   defp sample_filename("elixir"), do: "example.exs"
-   defp sample_filename("shell"), do: "example.sh"
-   defp sample_filename(_other), do: "example.txt"
-
-   entities = [{"&amp;", ?&}, {"&lt;", ?<}, {"&gt;", ?>}, {"&quot;", ?"}, {"&#39;", ?'}]
-
-   for {encoded, decoded} <- entities do
-     defp unescape_html(unquote(encoded) <> rest) do
-       [unquote(decoded) | unescape_html(rest)]
-     end
-   end
-
-   defp unescape_html(<<c, rest::binary>>) do
-     [c | unescape_html(rest)]
-   end
-
-   defp unescape_html(<<>>) do
-     []
-   end
-
-  @url 'http://localhost:9238/'
-  @content_type 'application/json'
-  @theme "Solarized (dark)"
-  def code(filename, code) do
-    request_body = Jason.encode!(%{
-      "filepath" => filename,
-      "theme" => @theme,
-      "code" => code
-    })
-    request = {@url, [], @content_type, request_body}
-    case :httpc.request(:post, request, [], []) do
-      {:ok, {{_, 200, _}, _headers, body}} ->
-        html = Jason.decode!(body) |> Map.get("data")
-        {:ok, html}
-      other ->
-        require Logger
-        Logger.error("Failed to highlight: #{inspect(other)}")
-        {:error, "failed to highlight"}
-    end
-  end
 end
