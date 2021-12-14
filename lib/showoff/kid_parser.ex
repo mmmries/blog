5ebbef1072bf0145133d43bdd73116299efdb4ae
Michael Ries <riesmmm@gmail.com>
2021-12-14 12:53:34 +0000

    use a whitelist of tags and special case circle

--- lib/showoff/kid_parser.ex
+++ lib/showoff/kid_parser.ex
@@ -2,9 +2,13 @@ defmodule Showoff.KidParser do
   import NimbleParsec
 
   def parse(str) do
-    with {:ok, raw, _, _, _, _} <- pparse(str),
-        tuples when is_list(tuples) <- convert_to_tuples(raw) do
-      {:ok, Enum.reverse(tuples)}
+    try do
+      with {:ok, raw, _, _, _, _} <- pparse(str),
+          tuples when is_list(tuples) <- convert_to_tuples(raw) do
+        {:ok, Enum.reverse(tuples)}
+      end
+    rescue
+      _ -> {:error, "invalid shape"}
     end
   end
 
@@ -24,18 +28,108 @@ defmodule Showoff.KidParser do
     fill: "black"
   }
 
-  @special_shapes [
+  @polygons [
     "triangle",
     "square",
     "pentagon",
     "hexagon",
     "octagon"
   ]
+
+  @svg_tags [
+    "a",
+    "altGlyph",
+    "altGlyphDef",
+    "altGlyphItem",
+    "animate",
+    "animateColor",
+    "animateMotion",
+    "animateTransform",
+    "animation",
+    "circle",
+    "clipPath",
+    "color-profile",
+    "cursor",
+    "defs",
+    "desc",
+    "discard",
+    "ellipse",
+    "feBlend",
+    "feColorMatrix",
+    "feComponentTransfer",
+    "feComposite",
+    "feConvolveMatrix",
+    "feDiffuseLighting",
+    "feDisplacementMap",
+    "feDistantLight",
+    "feDropShadow",
+    "feFlood",
+    "feFuncA",
+    "feFuncB",
+    "feFuncG",
+    "feFuncR",
+    "feGaussianBlur",
+    "feImage",
+    "feMerge",
+    "feMergeNode",
+    "feMorphology",
+    "feOffset",
+    "fePointLight",
+    "feSpecularLighting",
+    "feSpotLight",
+    "feTile",
+    "feTurbulence",
+    "filter",
+    "font",
+    "font-face",
+    "font-face-format",
+    "font-face-name",
+    "font-face-src",
+    "font-face-uri",
+    "foreignObject",
+    "g",
+    "glyph",
+    "glyphRef",
+    "handler",
+    "hkern",
+    "line",
+    "linearGradient",
+    "listener",
+    "marker",
+    "mask",
+    "metadata",
+    "missing-glyph",
+    "mpath",
+    "path",
+    "pattern",
+    "polygon",
+    "polyline",
+    "prefetch",
+    "radialGradient",
+    "rect",
+    "set",
+    "solidColor",
+    "stop",
+    "switch",
+    "symbol",
+    "tbreak",
+    "text",
+    "textArea",
+    "textPath",
+    "title",
+    "tref",
+    "tspan",
+    "unknown",
+    "use",
+    "view",
+    "vkern"
+  ]
+
   def convert_to_tuple([str]) when is_binary(str) do
     convert_to_tuple([str, []])
   end
 
-  def convert_to_tuple([shape, attributes]) when shape in @special_shapes do
+  def convert_to_tuple([shape, attributes]) when shape in @polygons do
     attributes = Enum.reduce(attributes, @default_attributes, fn [name, value], attrs ->
       name = maybe_to_atom(name)
       Map.put(attrs, name, value)
@@ -44,11 +138,23 @@ defmodule Showoff.KidParser do
     {shape, attributes}
   end
 
-  def convert_to_tuple([shape, attributes]) when is_binary(shape) do
+  def convert_to_tuple(["circle", attributes]) do
+    attributes = Enum.reduce(attributes, @default_attributes, fn [name, value], attrs ->
+      name = maybe_to_atom(name)
+      Map.put(attrs, name, value)
+    end)
+    {:circle, attributes, nil}
+  end
+
+  def convert_to_tuple([shape, attributes]) when shape in @svg_tags do
     attributes = Enum.reduce(attributes, %{}, fn [key, value], map ->
       Map.put(map, key, value)
     end)
     {shape, attributes, nil}
+  end
+
+  def convert_to_tuple([_shape, _attributes]) do
+    raise "invalid tag"
   end
 
   defp maybe_to_atom("cx"), do: :cx
