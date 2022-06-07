7e11dec8d3ef0c23dcba5b3c5afa224fd66aae08
Michael Ries <riesmmm@gmail.com>
2022-06-07 11:46:26 +0100

    update dependencies

--- config/config.exs
+++ config/config.exs
@@ -25,7 +25,7 @@ config :phoenix, :json_library, Jason
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.14.42",
   default: [
     args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
