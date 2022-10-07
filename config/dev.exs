38239c55e261c9de8cfaa7e2d3cd176055efb56f
Michael Ries <riesmmm@gmail.com>
2022-10-07 12:26:37 +0200

    get tailwind setup in the project

--- config/dev.exs
+++ config/dev.exs
@@ -13,7 +13,8 @@ config :blog, BlogWeb.Endpoint,
   check_origin: false,
   server: true,
   watchers: [
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
