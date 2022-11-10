1ce1fb91df4ca6f7ac098dfcb6f601cfad4f5495
Michael Ries <riesmmm@gmail.com>
2022-11-10 09:50:36 +0000

    get showoff working

--- lib/blog_web/components/sketches_component.ex
+++ lib/blog_web/components/sketches_component.ex
@@ -1,13 +1,12 @@
 defmodule BlogWeb.SketchesComponent do
-  use Phoenix.Component
-  alias BlogWeb.Router.Helpers, as: Routes
+  use BlogWeb, :html
 
   def list(assigns) do
     ~H"""
       <div class="flex flex-row items-center flex-wrap">
         <%= for sketch_id <- @sketch_ids do %>
           <div class="w-28 h-28 m-2 p-1 gb" phx-click="example" phx-value-id={sketch_id}>
-            <img src={Routes.showoff_path(@socket, :img, @room_name, sketch_id)} />
+            <img src={~p"/img/#{@room_name}/#{sketch_id}"} />
           </div>
         <% end %>
       </div>
