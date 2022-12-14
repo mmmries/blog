55ab13e3edcfb513ac624c4821fa1652daf79c29
Michael Ries <riesmmm@gmail.com>
2022-12-14 15:20:05 +0000

    user sketch rendering

--- test/showoff/showoff_controller_test.exs
+++ test/showoff/showoff_controller_test.exs
@@ -10,6 +10,16 @@ defmodule Showoff.ShowoffControllerTest do
     assert conn.resp_body == drawing.svg
   end
 
+  test "it renders user-submitted sketches" do
+    {:ok, drawing} = Showoff.kid_text_to_drawing("circle", "anonymous")
+    {:ok, sketch} = Showoff.RecentDrawings.add_drawing("controller_test", drawing)
+    conn = get(conn(), ~p{/img/controller_test/#{sketch.id}})
+
+    assert conn.status == 200
+    assert response_content_type(conn, :svg) == "image/svg+xml; charset=utf-8"
+    assert conn.resp_body == sketch.svg
+  end
+
   def conn do
     %Plug.Conn{build_conn() | host: "showoff.riesd.com"}
   end
