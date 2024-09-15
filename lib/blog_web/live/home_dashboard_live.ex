86437002a0a969f1f7f19941b6f914dac27519bb
Michael Ries <riesmmm@gmail.com>
2024-09-15 15:18:16 -0700

    display garage door status

--- lib/blog_web/live/home_dashboard_live.ex
+++ lib/blog_web/live/home_dashboard_live.ex
@@ -4,13 +4,20 @@ defmodule BlogWeb.HomeDashboardLive do
 
   use BlogWeb, :html_helpers
 
+  require Logger
+
   def mount(_args, session, socket) do
     username = Map.get(session, "current_user")
 
     socket =
       socket
       |> assign(:username, username)
+      |> assign(:garage_door_status, "pending")
       |> assign(:authorized?, authorized?(username))
+
+    if connected?(socket) do
+      Task.async(fn -> Home.GarageDoor.get_status() end)
+    end
 
     {:ok, socket}
   end
@@ -23,6 +30,22 @@ defmodule BlogWeb.HomeDashboardLive do
       {:error, msg} ->
         {:noreply, put_flash(socket, :error, msg)}
     end
+  end
+
+  # Get task results
+  def handle_info({ref, {:garage_door_status, status}}, socket) do
+    Process.demonitor(ref, [:flush])
+    {:noreply, assign(socket, garage_door_status: status)}
+  end
+
+  def handle_info({ref, {:error, reason}}, socket) do
+    Process.demonitor(ref, [:flush])
+    {:noreply, put_flash(socket, :error, "#{inspect(reason)}")}
+  end
+
+  def handle_info({:DOWN, _ref, :process, _pid, reason}, socket) do
+    Logger.error("Async task failed: #{inspect(reason)}")
+    {:noreply, socket}
   end
 
   defp authorized?(username) do
--- lib/home/garage_door.ex
+++ lib/home/garage_door.ex
@@ -1,4 +1,10 @@
 defmodule Home.GarageDoor do
+  def get_status do
+    with {:ok, message} <- Gnat.request(:gnat, "home.garage_door", "get_status") do
+      {:garage_door_status, message.body}
+    end
+  end
+
   def toggle do
     Gnat.pub(:gnat, "home.garage_door", "toggle")
   end
