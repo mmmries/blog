3167ba6d47620ebff3323933dca74f8edaa26a94
Michael Ries <riesmmm@gmail.com>
2025-11-08 15:33:53 -0800

    fix test

--- test/web/blog/homepage_test.exs
+++ test/web/blog/homepage_test.exs
@@ -21,13 +21,16 @@ defmodule BlogWeb.HomepageTest do
   test "search functionality works end-to-end", %{conn: conn} do
     {:ok, view, _html} = live(conn, ~p(/))
 
+    refute has_element?(view, "#search-results")
+
     # Test searching for content
     view
     |> form("form", search: "elixir")
     |> render_change()
 
     # Should have search results
-    assert has_element?(view, "a[href*='/']")
+    assert has_element?(view, "#search-results")
+    assert has_element?(view, "#search-results a[href*='/']")
 
     # Test clearing search
     view
@@ -35,26 +38,6 @@ defmodule BlogWeb.HomepageTest do
     |> render_change()
 
     # Results should be cleared
-    refute has_element?(view, ".absolute.top-full")
-  end
-
-  test "search blur functionality works", %{conn: conn} do
-    {:ok, view, _html} = live(conn, ~p(/))
-
-    # Search for something first
-    view
-    |> form("form#search", search: "elixir")
-    |> render_change()
-
-    # Should have results - check for the search results dropdown specifically
-    assert has_element?(view, "#search-results")
-
-    # Trigger blur event
-    view
-    |> element("input")
-    |> render_blur()
-
-    # Results should be cleared after the delay - check the dropdown is gone
     refute has_element?(view, "#search-results")
   end
 end
